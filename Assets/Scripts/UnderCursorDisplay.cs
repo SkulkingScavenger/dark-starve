a1e53c17a9b179c960e4f7b6c6a936e50715903c
seanstraus22 <sean.straus@macmillan.com>
2018-11-28 17:39:14 -0600

    setup construction preview management

--- Assets/Scripts/UnderCursorDisplay.cs
+++ Assets/Scripts/UnderCursorDisplay.cs
@@ -12,6 +12,11 @@ public class UnderCursorDisplay : MonoBehaviour {
 	GameObject root;
 	public bool overUI = false;
 
+	StructurePrototype structure = null;
+	StructurePrototype structurePrevious = null;
+	GameObject constructionPreview = null;
+	int constructionPreviewCellWidth = 64;
+
 	void Awake(){
 		itemIcons = Resources.LoadAll<Sprite>("Sprites/InventoryIcons");
 		heldItemImage = transform.Find("HeldItemImage").gameObject.GetComponent<Image>();
@@ -30,6 +35,15 @@ public class UnderCursorDisplay : MonoBehaviour {
 			transform.Find("HeldItemImage").localPosition = new Vector3(999,999,0);
 		}
 		overUI = CheckUICollision();
+
+
+		if(structure != structurePrevious){
+			DisplayConstructionPreview();
+		}
+		structurePrevious = structure;
+		if(constructionPreview != null){
+			constructionPreview.transform.position = new Vector3(transform.position.x,transform.position.y,-8);
+		}
 	}
 
 	bool CheckUICollision(){
@@ -43,9 +57,21 @@ public class UnderCursorDisplay : MonoBehaviour {
 	}
 
 	public void DisplayConstructionPreview(){
-		if(true){
-
+		if(constructionPreview != null){
+			Destroy(constructionPreview);
 		}
+		constructionPreview = Instantiate(Resources.Load<GameObject>("Prefabs/UI/ConstructionPreview"));
+		constructionPreview.GetComponent<SpriteRenderer>().sprite = structure.sprite;
+		GameObject gridCell = constructionPreview.transform.Find("GridCell").gameObject;
+		GameObject bumbel;
+		for(int i=0;i<structure.size;i++){
+			for(int j=0;j<structure.size;j++){
+				bumbel = Instantiate(gridCell);
+				bumbel.transform.SetParent(constructionPreview.transform);
+				bumbel.transform.localPosition = new Vector3((Decimal.Divide(structure.size-1,2) + i)*constructionPreviewCellWidth, (Decimal.Divide(structure.size-1,2) + j)*constructionPreviewCellWidth,0);
+			}
+		}
+		Destroy(gridCell);
 	}
 
 }
