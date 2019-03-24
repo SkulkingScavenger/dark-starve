5baea54d653774008d748c446a79a3dd7a152c5c
skulking <skulkingscavenger@gmail.com>
2019-03-24 12:40:29 -0500

    create gh-pages

--- Assets/Scripts/Structures.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StructurePrototype{
-	public Sprite sprite;
-	public string name = "";
-	public int size = 1;
-}
